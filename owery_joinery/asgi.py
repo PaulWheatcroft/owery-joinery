c9706a16ebf0c088b293adb01c37cea21e2e6d78
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-09-28 13:30:15 +0000

    Initial commit

---
+++ owery_joinery/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for owery_joinery project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'owery_joinery.settings')
+
+application = get_asgi_application()
