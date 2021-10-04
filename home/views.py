87144436c0cd42a1289a8f5154a4b4a8ad82c42b
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-10-04 19:36:48 +0000

    Messages and Bootrap toasts implemented

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 
 def index(request):
+    messages.success(request, 'Something went wrong!.')
+
     """ A view to return the index page """
     return render(request, 'home/index.html')
