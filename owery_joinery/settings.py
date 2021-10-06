6fb8771905fe375f17e15274d42e17cef6bd0b7f
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-10-06 20:49:12 +0000

    Added devlopment debug if statement in settings.py

--- owery_joinery/settings.py
+++ owery_joinery/settings.py
@@ -26,7 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if 'DEVELOPMENT' in os.environ:
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['owery-joinery.herokuapp.com', 'localhost']
 
