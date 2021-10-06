178cebb044c9832d4197a54de250555ad5875d97
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-10-06 20:37:42 +0000

    if statement added to switch databases depending on environment

--- owery_joinery/settings.py
+++ owery_joinery/settings.py
@@ -95,12 +95,16 @@ LOGIN_REDIRECT_URL = '/'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
