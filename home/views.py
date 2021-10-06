59ebfbaf32bfdcdea02b1b22af9bf389232d70ea
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-10-06 17:51:40 +0000

    Set up AWS for static files

--- home/views.py
+++ home/views.py
@@ -4,6 +4,6 @@ from django.contrib import messages
 
 def index(request):
     """ Load index page """
-    messages.success(request, 'Something went wrong!.')
+    messages.info(request, 'Testing alerts show correctly.')
 
     return render(request, 'home/index.html')
