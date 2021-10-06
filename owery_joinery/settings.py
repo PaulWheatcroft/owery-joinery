2f3a49a80d007901629612e68598829bdf08129b
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-10-06 20:21:45 +0000

    Hardcoded MEDIA_URL path

--- owery_joinery/settings.py
+++ owery_joinery/settings.py
@@ -175,7 +175,7 @@ if 'USE_AWS' in os.environ:
 
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    MEDIA_URL = 'https://owery-joinery.s3.eu-west-2.amazonaws.com/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
