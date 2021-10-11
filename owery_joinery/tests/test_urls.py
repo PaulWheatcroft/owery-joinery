e7c63c46a25cb7ab98b4a7ee61a0d7b5928f0a99
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-10-11 14:29:25 +0000

    Django tests added for product and filtered products urls

--- owery_joinery/tests/test_urls.py
+++ owery_joinery/tests/test_urls.py
@@ -1,6 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from home.views import index
+from products.views import all_products, filtered_products
 from django.contrib import admin
 
 
@@ -11,3 +12,12 @@ class TestUrls(SimpleTestCase):
         url = reverse('home')
         self.assertEqual(resolve(url).func, index)
 
+    def test_products_page_is_resolved(self):
+        """ testing products.html """
+        url = reverse('products')
+        self.assertEqual(resolve(url).func, all_products)
+
+    def test_products_filtered_page_is_resolved(self):
+        """ testing products.html """
+        url = reverse('filtered_products', args=1)
+        self.assertEqual(resolve(url).func, filtered_products)
