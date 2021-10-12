ed741a2d631005e3b058fb27f7fa7d35455397ac
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-10-12 17:30:37 +0000

    Styling the product details page

--- home/views.py
+++ home/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-from django.contrib import messages
 from products.models import Category
 
 
 def index(request):
     """ A view to return the index page """
-    messages.info(request, 'Testing alerts show correctly.')
 
     categories = Category.objects.all()
 
