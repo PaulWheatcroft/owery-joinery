00982ba43c038e4b6be593701b85bff4b4155a9e
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-10-13 09:35:45 +0000

    Added checkout app and models

--- owery_joinery/tests/test_urls.py
+++ owery_joinery/tests/test_urls.py
@@ -1,8 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from home.views import index
-from products.views import all_products, filtered_products
-from django.contrib import admin
+from products.views import filtered_products
 
 
 class TestUrls(SimpleTestCase):
@@ -12,12 +11,7 @@ class TestUrls(SimpleTestCase):
         url = reverse('home')
         self.assertEqual(resolve(url).func, index)
 
-    def test_products_page_is_resolved(self):
-        """ testing all products being resolved """
-        url = reverse('products')
-        self.assertEqual(resolve(url).func, all_products)
-
     def test_products_filtered_page_is_resolved(self):
         """ testing products fiiltered by category id 3 """
         url = reverse('filtered_products', args=1)
-        self.assertEqual(resolve(url).func, filtered_products)
+        self.assertEqual(resolve(url).func, filtered_products('', 1))
