37a867179537b155d0116548c5565f4b2f6deae8
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-10-13 14:21:25 +0000

    Basic Stripe functionality implemented

--- checkout/views.py
+++ checkout/views.py
@@ -1,23 +1,37 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.conf import settings
+from cart.contexts import contents_of_cart
 
 from .forms import OrderForm
 
+import stripe
+
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "Your shopping cart is empty")
         return redirect(reverse('products'))
 
+    current_cart = contents_of_cart(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
-    stripe_public_key = 'STRIPE_PUBLIC_KEY'
-    client_secret = 'CLIENT_SECRET'
+
     context = {
        'order_form': order_form,
        'stripe_public_key': stripe_public_key,
-       'client_secret': client_secret,
+       'client_secret': intent.client_secret
     }
 
     return render(request, 'checkout/checkout.html', context)
--- owery_joinery/settings.py
+++ owery_joinery/settings.py
@@ -191,5 +191,7 @@ if 'USE_AWS' in os.environ:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+# Stripe
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
