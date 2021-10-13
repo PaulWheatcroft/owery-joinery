261cd439b7c9b444e6bf6eab07c51ef54fe53a36
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-10-13 17:45:07 +0000

    POST checkout form configured

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 from cart.contexts import contents_of_cart
 
 from .forms import OrderForm
+from .models import Order, OrderLineItems
+from products.models import Product
 
 import stripe
 
@@ -12,26 +14,51 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    cart = request.session.get('cart', {})
-    if not cart:
-        messages.error(request, "Your shopping cart is empty")
-        return redirect(reverse('products'))
+    if request.method == 'POST':
+        cart = request.session.get('cart', {})
 
-    current_cart = contents_of_cart(request)
-    total = current_cart['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        form_data = {
+            'first_name': request.POST['first_name'],
+            'last_name': request.POST['last_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'town_or_city': request.POST['town_or_city'],
+            'postcode': request.POST['postcode'],
+            'county': request.POST['county'],
+            'country': request.POST['country'],
+        }
+        order_form = OrderForm(form_data)
+        print('***')
+        print(order_form)
+        print('***')
+        print(form_data)
+        print('***')
+        for product_id, quantity in cart.items():
+            print(product_id, quantity)
+            print('******')
 
-    order_form = OrderForm()
+    else:
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(request, "Your shopping cart is empty")
+            return redirect(reverse('products'))
 
-    context = {
-       'order_form': order_form,
-       'stripe_public_key': stripe_public_key,
-       'client_secret': intent.client_secret
-    }
+        current_cart = contents_of_cart(request)
+        total = current_cart['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+        
+        order_form = OrderForm()
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret
+        }
 
-    return render(request, 'checkout/checkout.html', context)
+        return render(request, 'checkout/checkout.html', context)
