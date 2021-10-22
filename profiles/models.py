293309656da13651444f0aa00cadd86215e08082
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-10-22 23:53:21 +0000

    Added allauth reciever in profiles model

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+
+from allauth.account.signals import email_confirmed
 
 
 class UserProfile(models.Model):
@@ -7,7 +10,16 @@ class UserProfile(models.Model):
     A user profile model for maintaining order history
     """
     user_profile = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    profile_phone_number = models.CharField(
+        max_length=20, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(email_confirmed)
+def email_confirmed_(instance, **kwargs):
+    """
+    Create user profile
+    """
+    UserProfile.objects.create(user=instance)
