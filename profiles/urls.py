90fb86832d416138826a98fb5ace981e3950d32f
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-10-23 13:22:30 +0000

    Order details page created

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import profile
+from .views import profile, view_order
 
 urlpatterns = [
-    path('', profile, name='profile')
+    path('', profile, name='profile'),
+    path('order/<order_number>/', view_order, name='view_order'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,18 +1,39 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import UserProfile
 
-from checkout.models import Order
+from checkout.models import Order, OrderLineItems
 
 
 def profile(request):
-    """ A view to return the logged in user's profile page """
+    """
+    A view to return the logged in user's profile page
+    which contains a list of their previous orders
+    """
     if request.user.is_authenticated:
         logged_in_profile = get_object_or_404(UserProfile, user=request.user)
-        print(logged_in_profile)
 
         orders = Order.objects.filter(user_profile=logged_in_profile)
-        print(orders)
 
-        return render(request, 'profiles/profile.html')
+        context = {
+            'logged_in_profile': logged_in_profile,
+            'orders': orders,
+        }
+
+        return render(request, 'profiles/profile.html', context)
     else:
-        return redirect('account_signup') 
+        return redirect('account_signup')
+
+
+def view_order(request, order_number):
+    """
+    A view to return an order;s details
+    """
+    order = Order.objects.filter(order_number=order_number)
+    line_items = OrderLineItems.objects.filter(order=order[0].id)
+
+    context = {
+        'order': order,
+        'line_items': line_items
+    }
+
+    return render(request, 'profiles/order_details.html', context)
