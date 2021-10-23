b6c600abc01548e33aa904f3ffaa823706f4060e
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-10-23 09:56:00 +0000

    Profile view returns orders for a user if authenticated if not redirect to sign-up page

--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import UserProfile
+
+from checkout.models import Order
 
 
 def profile(request):
     """ A view to return the logged in user's profile page """
-    profile = get_object_or_404(UserProfile, user=request.user)
-    print(profile)
+    if request.user.is_authenticated:
+        logged_in_profile = get_object_or_404(UserProfile, user=request.user)
+        print(logged_in_profile)
 
-    orders = profile.orders.all()
+        orders = Order.objects.filter(user_profile=logged_in_profile)
+        print(orders)
 
-    print(orders)
-
-    return render(request, 'profiles/profile.html')
+        return render(request, 'profiles/profile.html')
+    else:
+        return redirect('account_signup') 
